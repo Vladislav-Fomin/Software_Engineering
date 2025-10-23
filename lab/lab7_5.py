0f79874a308b352c0e51c2dd49a5d21d0b63612e
Vladislav-Fomin <105593638+Vladislav-Fomin@users.noreply.github.com>
2025-10-23 23:16:44 +0500

    Add files via upload

---
+++ lab/lab7_5.py
@@ -0,0 +1,4 @@
+with open('input1.txt') as f:
+    for line in f:
+        print(line)
+
---
+++ lab/lab7_6.py
@@ -0,0 +1,6 @@
+with open('input1.txt', 'a+') as f:
+    f.write('\nIm additional line')
+
+with open('input.txt', 'r') as f:
+    result = f.readlines()
+    print(result)
